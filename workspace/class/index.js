40349f15a53135efe37f278f2b91ba4a9d18c8d7
Bertrand SIFRE <Bertrand.SIFRE@lacooperativewelcoop.com>
2022-08-13 22:35:44 +0200

    add class exposition

---
+++ workspace/class/index.js
@@ -0,0 +1,26 @@
+const f = require('cutl-knucklebones-functions')
+
+class Game {
+  constructor() {
+    /** @type {import('cutl-knucklebones-functions').Board} */
+    this.board = f.initBoard()
+  }
+
+  getPlayer1Point() {
+    return f.getPlayer1Point(this.board)
+  }
+
+  getPlayer2Point() {
+    return f.getPlayer2Point(this.board)
+  }
+
+  playPlayer1(diceValue, column) {
+    return f.playPlayer1(this.board, diceValue, column)
+  }
+
+  playPlayer2(diceValue, column) {
+    return f.playPlayer2(this.board, diceValue, column)
+  }
+}
+
+module.exports = Game
---
+++ workspace/class/index.spec.js
@@ -0,0 +1,28 @@
+const Game = require('./index')
+
+describe('Board', () => {
+  it('Basic points', () => {
+    const game = new Game()
+    game.board = [[[1, 2, 3], [1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6], [4, 5, 6]]]
+
+    const p1Points = game.getPlayer1Point()
+    const p2Points = game.getPlayer2Point()
+    expect(p1Points).toEqual(18)
+    expect(p2Points).toEqual(45)
+  })
+  it('Double points', () => {
+    const game = new Game()
+    game.board = [[[1, 1, 1], [2, 3, 2], [4, 4, 3]], [[4, 5, 1], [4, 6], [4]]]
+
+    const p1Points = game.getPlayer1Point()
+    const p2Points = game.getPlayer2Point()
+    expect(p2Points).toEqual(24)
+    expect(p1Points).toEqual(42)
+  })
+  it('player 1 play', () => {
+    const game = new Game()
+    game.playPlayer1(1, 1)
+    expect(game.getPlayer1Point()).toEqual(1)
+    expect(game.getPlayer2Point()).toEqual(0)
+  })
+})
