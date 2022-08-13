a5dc59c193ee416891d282e7793d0ba789e2cbf5
Bertrand SIFRE <Bertrand.SIFRE@lacooperativewelcoop.com>
2022-08-13 21:36:46 +0200

    feat(optimization): replace class by functions

--- src/game.js
+++ src/game.js
@@ -12,72 +12,91 @@ const { countBy } = require('lodash')
  * @typedef {[Column, Column, Column]} PlayerBoard
  */
 
-class Game {
-  constructor() {
-    /** @type {[PlayerBoard, PlayerBoard]} */
-    this.board = [[[], [], []], [[], [], []]]
-  }
+/**
+ * @typedef {[PlayerBoard, PlayerBoard]} Board
+ */
 
-  /**
-   * @param {number} player 
-   * @param {DiceValue} diceValue 
-   * @param {number} column 
-   */
-  #play(player, diceValue, column) {
-    if (player < 1 || player > 2) {
-      throw new Error('Player must be 1 or 2')
-    }
-    if (diceValue < 1 || diceValue > 6) {
-      throw new Error('The dice is a D6')
-    }
-    if (column < 1 || column > 3) {
-      throw new Error('The column is 1, 2 or 3')
-    }
-    const boardPlayer = this.board[player - 1]
-    if (boardPlayer[column - 1].length === 3) {
-      throw new Error('You cannot play here')
-    }
-    const c = boardPlayer[column - 1]
-    //@ts-ignore
-    c.push(diceValue)
-  }
-
-  /**
-   * @param {DiceValue} diceValue 
-   * @param {number} column 
-   */
-  playPlayer1(diceValue, column) {
-    this.#play(1, diceValue, column)
-  }
-
-  /**
-   * @param {DiceValue} diceValue 
-   * @param {number} column 
-   */
-  playPlayer2(diceValue, column) {
-    this.#play(2, diceValue, column)
-  }
-
-  /**
-   * @param {number[][]} playerBoard 
-   */
-  #getPoint(playerBoard) {
-    return playerBoard.reduce((totalPoint, column) => {
-      const groupBy = countBy(column)
-      return totalPoint + Object.keys(groupBy).reduce((columnPoint, key) => {
-        const pt = Number.parseInt(key)
-        return columnPoint + pt * groupBy[key] * Math.pow(2, groupBy[key] - 1)
-      }, 0)
-    }, 0)
-  }
-
-  getPlayer1Point() {
-    return this.#getPoint(this.board[0])
-  }
-
-  getPlayer2Point() {
-    return this.#getPoint(this.board[1])
-  }
+/**
+ * @returns {Board}
+ */
+const initBoard = function () {
+  return [[[], [], []], [[], [], []]]
 }
 
-module.exports = Game
+/**
+ * @param {Board} board
+ * @param {number} player 
+ * @param {DiceValue} diceValue 
+ * @param {number} column 
+ */
+const play = function (board, player, diceValue, column) {
+  if (player < 1 || player > 2) {
+    throw new Error('Player must be 1 or 2')
+  }
+  if (diceValue < 1 || diceValue > 6) {
+    throw new Error('The dice is a D6')
+  }
+  if (column < 1 || column > 3) {
+    throw new Error('The column is 1, 2 or 3')
+  }
+  const boardPlayer = board[player - 1]
+  if (boardPlayer[column - 1].length === 3) {
+    throw new Error('You cannot play here')
+  }
+  const c = boardPlayer[column - 1]
+  //@ts-ignore
+  c.push(diceValue)
+}
+
+/**
+ * @param {Board} board
+ * @param {DiceValue} diceValue 
+ * @param {number} column 
+ */
+const playPlayer1 = function (board, diceValue, column) {
+  play(board, 1, diceValue, column)
+}
+
+/**
+ * @param {Board} board
+ * @param {DiceValue} diceValue 
+ * @param {number} column 
+ */
+const playPlayer2 = function (board, diceValue, column) {
+  play(board, 2, diceValue, column)
+}
+
+/**
+ * @param {number[][]} playerBoard 
+ */
+const getPoint = function (playerBoard) {
+  return playerBoard.reduce((totalPoint, column) => {
+    const groupBy = countBy(column)
+    return totalPoint + Object.keys(groupBy).reduce((columnPoint, key) => {
+      const pt = Number.parseInt(key)
+      return columnPoint + pt * groupBy[key] * Math.pow(2, groupBy[key] - 1)
+    }, 0)
+  }, 0)
+}
+
+/**
+ * @param { Board } board
+ */
+const getPlayer1Point = function (board) {
+  return getPoint(board[0])
+}
+
+/**
+ * @param { Board } board
+ */
+const getPlayer2Point = function (board) {
+  return getPoint(board[1])
+}
+
+module.exports = {
+  initBoard,
+  getPlayer1Point,
+  getPlayer2Point,
+  playPlayer1,
+  playPlayer2,
+}
--- src/game.spec.js
+++ src/game.spec.js
@@ -1,28 +1,29 @@
-const Game = require('./game')
+const game = require('./game')
 
 describe('Board', () => {
   it('Basic points', () => {
-    const game = new Game()
-    game.board = [[[1, 2, 3], [1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6], [4, 5, 6]]]
+    /** @type {import('./game').Board} */
+    const board = [[[1, 2, 3], [1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6], [4, 5, 6]]]
 
-    const p1Points = game.getPlayer1Point()
-    const p2Points = game.getPlayer2Point()
+    const p1Points = game.getPlayer1Point(board)
+    const p2Points = game.getPlayer2Point(board)
     expect(p1Points).toEqual(18)
     expect(p2Points).toEqual(45)
   })
   it('Double points', () => {
-    const game = new Game()
-    game.board = [[[1, 1, 1], [2, 3, 2], [4, 4, 3]], [[4, 5, 1], [4, 6], [4]]]
+    /** @type {import('./game').Board} */
+    const board = [[[1, 1, 1], [2, 3, 2], [4, 4, 3]], [[4, 5, 1], [4, 6], [4]]]
 
-    const p1Points = game.getPlayer1Point()
-    const p2Points = game.getPlayer2Point()
+    const p1Points = game.getPlayer1Point(board)
+    const p2Points = game.getPlayer2Point(board)
     expect(p2Points).toEqual(24)
     expect(p1Points).toEqual(42)
   })
   it('player 1 play', () => {
-    const game = new Game()
-    game.playPlayer1(1, 1)
-    expect(game.getPlayer1Point()).toEqual(1)
-    expect(game.getPlayer2Point()).toEqual(0)
+    /** @type {import('./game').Board} */
+    const board = game.initBoard()
+    game.playPlayer1(board, 1, 1)
+    expect(game.getPlayer1Point(board)).toEqual(1)
+    expect(game.getPlayer2Point(board)).toEqual(0)
   })
 })
